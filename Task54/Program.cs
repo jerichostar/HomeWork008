0c362833ad8c0aa558333f29c75da245334c497d
jerichostar <103570234+jerichostar@users.noreply.github.com>
2023-01-09 19:59:26 +0300

    Task54 Done

---
+++ Task54/Program.cs
@@ -0,0 +1,67 @@
+﻿/* Задача 54: Задайте двумерный массив. 
+Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+
+*/
+
+int[,] GetArrayRandomItemsTwoDimension(int xSizeArray = 4,
+                                        int ySizeArray = 4,
+                                        int startRndNum = 1,
+                                        int finishRndNum = 10)
+{
+    int[,] array = new int[xSizeArray, ySizeArray];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(startRndNum, finishRndNum);
+        }
+    }
+    return array;
+}
+
+void PrintTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SortArrayRowMaxToMin(int[,] array)
+{
+    int[,] sortedArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array = GetArrayRandomItemsTwoDimension();
+PrintTwoDimensionalArray(array);
+SortArrayRowMaxToMin(array);
+PrintTwoDimensionalArray(array);
